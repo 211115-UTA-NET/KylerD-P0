c6ccf62e74b16e1301c1ab1e7d72f068e27c44c1
Kyler Dennis <KylerGDennis@gmail.com>
2021-12-19 23:44:10 -0500

    Customers can now remove items from their cart

--- SpiceItUp/CustomerOrder.cs
+++ SpiceItUp/CustomerOrder.cs
@@ -12,6 +12,7 @@ namespace SpiceItUp
         private static string connectionString = File.ReadAllText("D:/Revature/ConnectionStrings/SpiceItUp-P0-KylerD.txt");
         private static int storeEntry;
         private static string? storeName;
+        private static bool exit = false;
 
         private static List<int> itemIDList = new List<int>();
         private static List<string> itemNameList = new List<string>();
@@ -25,7 +26,8 @@ namespace SpiceItUp
 
         public static void StoreSelection()
         {
-            while (true)
+            exit = false;
+            while (exit == false)
             {
                 Console.WriteLine("Enter a store number to begin shopping:");
 
@@ -103,7 +105,6 @@ namespace SpiceItUp
 
         private static void AddToCart()
         {
-            bool exit = false;
             while (exit == false)
             {
                 Console.WriteLine($"Store {storeEntry}: {storeName}");
@@ -187,7 +188,7 @@ namespace SpiceItUp
                             else if (validQuantity == true && quantity > inStockList[itemToAdd])
                                 Console.WriteLine("This store does not have enough in stock for that quantity. \nPlease enter a new quantity:");
                             else if (validQuantity == true && quantity < 1)
-                                Console.WriteLine("You cannot hava a quantity less than 1. \nPlease enter a new quantity:");
+                                Console.WriteLine("You cannot have a quantity less than 1. \nPlease enter a new quantity:");
                             else
                                 Console.WriteLine("Sorry, quantities are limited to 10 per item. \nPlease enter a new quantity.");
                         }
@@ -198,8 +199,127 @@ namespace SpiceItUp
                     else if (validEntry == true && itemToAdd > -1 && itemToAdd < itemIDList.Count && inStockList[itemToAdd] == 0)
                         Console.WriteLine("We are currently out of this item. \nPlease enter a new item ID:");
                     else if (validEntry == true && itemToAdd == -1)
-                        ViewCustomerCart();
+                    {
+                        if (customerItemID.Count == 0)
+                            Console.WriteLine("Your cart is empty. Enter a valid Item ID to create a cart:");
+                        else
+                        {
+                            ViewCustomerCart();
+                            break;
+                        }
+                    }
                     else if (adding == "EXIT")
+                    {
+                        itemIDList.Clear();
+                        itemNameList.Clear();
+                        inStockList.Clear();
+                        priceList.Clear();
+                        customerItemID.Clear();
+                        customerItemName.Clear();
+                        customerQuantity.Clear();
+                        customerPrice.Clear();
+                        exit = true;
+                        break;
+                    }
+                    else
+                        Console.WriteLine("There was an error. \nPlease enter a new item ID:");
+                }
+            }
+        }
+
+        private static void RemoveFromCart()
+        {
+            while (exit == false)
+            {
+                if (customerItemID.Count == 0)
+                {
+                    Console.WriteLine("Your cart is now empty.");
+                    break;
+                }
+                Console.WriteLine($"Cart for store {storeEntry}: {storeName}");
+                Console.WriteLine("=========================================");
+                Console.WriteLine("Item ID\t Item Name\t Quantity\t Price");
+                Console.WriteLine("=======\t =========\t ========\t =====");
+                for (int i = 0; i < customerItemID.Count; i++)
+                {
+                    string price = String.Format("{0:0.00}", customerPrice[i]);
+                    Console.WriteLine(String.Format("{0, -8} {1, -15} {2, -15} {3, -16}",
+                        customerItemID[i], customerItemName[i], customerQuantity[i], $"${price}"));
+                }
+                Console.WriteLine("=========================");
+                Console.WriteLine("Enter an Item ID to remove it from your cart, or enter 0 to view your cart.");
+                Console.WriteLine("Type 'EXIT' to cancel your order and return to your account menu.");
+                while (true)
+                {
+                    int itemToRemove;
+                    string? removing = Console.ReadLine();
+                    bool validEntry = int.TryParse(removing, out itemToRemove);
+                    itemToRemove--;
+                    if (validEntry == true && itemToRemove > -1)
+                    {
+                        bool failedEntry = true;
+                        Console.WriteLine("Enter a quantity to remove from your cart:");
+                        while (failedEntry == true)
+                        {
+                            string? quantityString = Console.ReadLine();
+                            int quantity;
+                            bool validQuantity = int.TryParse(quantityString, out quantity);
+                            if (validQuantity == true && quantity > 0 && quantity < 11)
+                            {
+                                itemToRemove++;
+                                int itemRemoved = 0;
+                                for (int j = 0; j < customerItemID.Count; j++)
+                                {
+                                    if (itemToRemove == customerItemID[j])
+                                    {
+                                        itemToRemove--;
+                                        int oldQuantity = customerQuantity[j];
+                                        int finalQuantity = oldQuantity - quantity;
+                                        if (finalQuantity < 0)
+                                        {
+                                            Console.WriteLine("You cannot remove more than what you have. Please enter a new quantity:");
+                                            break;
+                                        }
+                                        else if (finalQuantity == 0)
+                                        {
+                                            failedEntry = false;
+                                            customerItemID.RemoveAt(j);
+                                            customerItemName.RemoveAt(j);
+                                            customerQuantity.RemoveAt(j);
+                                            customerPrice.RemoveAt(j);
+                                            inStockList[itemToRemove] = inStockList[itemToRemove] + quantity;
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            failedEntry = false;
+                                            decimal itemPrice = customerPrice[j] / customerQuantity[j];
+                                            customerQuantity[j] = customerQuantity[j] - quantity;
+                                            decimal newPrice = itemPrice * customerQuantity[j];
+                                            customerPrice[j] = newPrice;
+                                            inStockList[itemToRemove] = inStockList[itemToRemove] + quantity;
+                                            break;
+                                        }
+                                    }
+                                    else
+                                        itemRemoved++;
+                                }
+                                if (itemRemoved == customerItemID.Count)
+                                    Console.WriteLine("We could not find that item. Please try again.");
+                                break;
+                            }
+                            else if (validQuantity == true && quantity <= 0)
+                                Console.WriteLine("You cannot remove a quantity less than 1. \nPlease enter a new quantity to remove:");
+                            else if (validQuantity == true && quantity > 10)
+                                Console.WriteLine("You cannot remove a quantity more than 10. \nPlease enter a new quantity to remove:");
+                            else
+                                Console.WriteLine("There was an error. \nPlease enter a new quantity to remove:");
+                        }
+                        break;
+                    }
+                    else if (validEntry == true && itemToRemove == -1)
+                        ViewCustomerCart();
+                    else if (removing == "EXIT")
                     {
                         itemIDList.Clear();
                         itemNameList.Clear();
@@ -235,6 +355,46 @@ namespace SpiceItUp
             string stringTotalPrice = String.Format("{0:0.00}", totalPrice);
             Console.WriteLine("=========================");
             Console.WriteLine($"Your total is ${stringTotalPrice}");
+            Console.WriteLine("=========================");
+            Console.WriteLine("How would you like to continue?");
+            Console.WriteLine("1: Add more items to my cart \n2: Remove items from my cart \n3: Finalize transaction and checkout \n4: Cancel transaction and return to menu");
+
+            int userEntry;
+            while (true) //Test to ensure user entry is valid
+            {
+                string? mySelection = Console.ReadLine();
+                bool validEntry = int.TryParse(mySelection, out userEntry);
+                if (validEntry == true && userEntry >= 1 && userEntry <= 4)
+                {
+                    break; //Break when valid
+                }
+                else
+                    Console.WriteLine("Invalid selection. Please try again.");
+            }
+
+            switch (userEntry)
+            {
+                case 1:
+                    AddToCart();
+                    break;
+                case 2:
+                    RemoveFromCart();
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    itemIDList.Clear();
+                    itemNameList.Clear();
+                    inStockList.Clear();
+                    priceList.Clear();
+                    customerItemID.Clear();
+                    customerItemName.Clear();
+                    customerQuantity.Clear();
+                    customerPrice.Clear();
+                    exit = true;
+                    break;
+            }
 
             StringBuilder createTransID = new StringBuilder();
             Enumerable
@@ -246,7 +406,6 @@ namespace SpiceItUp
                .Take(11)
                .ToList().ForEach(e => createTransID.Append(e));
             string transID = createTransID.ToString();
-            Console.ReadLine();
         }
     }
 }
